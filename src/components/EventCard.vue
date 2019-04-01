20702be7d8eda1d34b5cbc7ae673589a4e54653c
arre1908 <arredoluis@gmail.com>
2019-04-01 18:50:23 -0500

    update configurations and other

---
+++ src/components/EventCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input v-model="quote" type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: "I want to be"
+    };
+  }
+};
+</script>
+
+<style scoped></style>
