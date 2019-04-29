0b302f428c30ed334f5728af54137f032f21d5d4
arre1908 <arredoluis@gmail.com>
2019-04-28 22:41:32 -0500

    added components with named slots

---
+++ src/components/MediaBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <slot name="heading"></slot>
+    <slot name="paragraph"></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  background-color: aliceblue;
+  padding-left: 5px;
+}
+</style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,15 +2,24 @@
   <div>
     <h1>Event Listing</h1>
     <EventCard />
+    <MediaBox>
+      <h3 slot="heading">Named slot 1</h3>
+      <template slot="paragraph">
+        <h4>Named slot 2</h4>
+        <p>Also named slot 2</p>
+      </template>
+    </MediaBox>
   </div>
 </template>
 
 <script>
 import EventCard from "@/components/EventCard.vue";
+import MediaBox from "@/components/MediaBox.vue";
 
 export default {
   components: {
-    EventCard
+    EventCard,
+    MediaBox
   }
 };
 </script>
